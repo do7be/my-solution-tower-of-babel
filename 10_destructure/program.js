483f37c118a094d1390b9e3d45585af99581ada5
do7be <bomsap700@gmail.com>
2015-11-07 11:53:01 +0900

    add 10 destructure

---
+++ 10_destructure/program.js
@@ -0,0 +1,17 @@
+let json = {
+  "name": {
+    "first": "Yosuke",
+    "family": process.argv[2]
+  },
+  "birth": {
+    "year": 1982,
+    "month": 12,
+    "day": process.argv[3]
+  }
+};
+
+let familyName, birthDay;
+[familyName, birthDay] = [json.name.family, json.birth.day];
+
+console.log(familyName);
+console.log(birthDay);
